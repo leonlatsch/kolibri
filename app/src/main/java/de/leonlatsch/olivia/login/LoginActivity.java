b14c2d33d98d09ca213e235d79ff1eff88e4e136
Leon Latsch <leonlatsch@gmx.de>
2019-05-25 15:00:23 +0200

    Added email caching when switching to register

--- app/src/main/java/de/leonlatsch/olivia/login/LoginActivity.java
+++ app/src/main/java/de/leonlatsch/olivia/login/LoginActivity.java
@@ -1,10 +1,14 @@
 package de.leonlatsch.olivia.login;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import de.leonlatsch.olivia.R;
+import de.leonlatsch.olivia.register.RegisterActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -18,5 +22,24 @@ public class LoginActivity extends AppCompatActivity {
 
         emailEditText = findViewById(R.id.loginEmailEditText);
         passwordEditText = findViewById(R.id.loginPasswordEditText);
+
+        final Button register = findViewById(R.id.loginRegisterNowBtn);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                cacheToIntent(intent);
+                startActivity(intent);
+            }
+        });
+        }
+
+        private void cacheToIntent(Intent intent) {
+        String email = emailEditText.getText().toString();
+
+        if (email != null && !email.isEmpty()) {
+            intent.putExtra(getString(R.string.loginEmail), email);
+        }
     }
 }
--- app/src/main/java/de/leonlatsch/olivia/register/RegisterActivity.java
+++ app/src/main/java/de/leonlatsch/olivia/register/RegisterActivity.java
@@ -1,15 +1,40 @@
 package de.leonlatsch.olivia.register;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
 import de.leonlatsch.olivia.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private EditText registerUsernameEditText;
+    private EditText registerEmailEditText;
+    private EditText registerPasswordEditText;
+    private EditText registerPasswordConfirmEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        registerUsernameEditText = findViewById(R.id.registerUsernameEditText);
+        registerEmailEditText = findViewById(R.id.registerEmailEditText);
+        registerPasswordEditText = findViewById(R.id.registerPasswordEditText);
+        registerPasswordConfirmEditText = findViewById(R.id.registerPasswordConfirmEditText);
+
+        loadCachedData();
+
+        registerUsernameEditText.requestFocus();
+    }
+
+    private void loadCachedData() {
+        if (getIntent().getExtras() != null) {
+            String cachedEmail = (String) getIntent().getExtras().get(getString(R.string.loginEmail));
+
+            if (cachedEmail != null) {
+                registerEmailEditText.setText(cachedEmail);
+            }
+        }
     }
 }
