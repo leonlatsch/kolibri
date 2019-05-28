45a88b94cbd01974e111fe5dc94fd2105676a29d
Leon Latsch <leonlatsch@gmx.de>
2019-05-28 14:07:55 +0200

    Added imports; Need to redo authentification

--- app/src/main/java/de/leonlatsch/olivia/rest/http/OliviaHttpClient.java
+++ app/src/main/java/de/leonlatsch/olivia/rest/http/OliviaHttpClient.java
@@ -1,4 +1,17 @@
-public class OliviaHttpClient {  
+package de.leonlatsch.olivia.rest.http;
+
+import java.security.cert.CertificateException;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import okhttp3.OkHttpClient;
+
+public class OliviaHttpClient {
     public static OkHttpClient getOliviaHttpClient() {
         try {
             // Create a trust manager that does not validate certificate chains
