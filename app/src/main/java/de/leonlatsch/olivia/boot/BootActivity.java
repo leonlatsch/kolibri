37f6cb98285d9948a4cee164d4520377b8c455aa
Leon Latsch <leonlatsch@gmx.de>
2019-06-23 14:58:29 +0200

    Added boot activity and set as startup

---
+++ app/src/main/java/de/leonlatsch/olivia/boot/BootActivity.java
@@ -0,0 +1,15 @@
+package de.leonlatsch.olivia.boot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import de.leonlatsch.olivia.R;
+
+public class BootActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_boot);
+    }
+}
