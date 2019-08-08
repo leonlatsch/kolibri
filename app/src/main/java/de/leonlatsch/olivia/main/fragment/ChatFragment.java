6d1c2e7229e51b32536f71b07a08cd8c48126cba
Leon Latsch <leonlatsch@gmx.de>
2019-08-08 21:23:11 +0200

    added button and onclick

--- app/src/main/java/de/leonlatsch/olivia/main/fragment/ChatFragment.java
+++ app/src/main/java/de/leonlatsch/olivia/main/fragment/ChatFragment.java
@@ -8,13 +8,30 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import de.leonlatsch.olivia.R;
+import de.leonlatsch.olivia.main.MainActivity;
 
 public class ChatFragment extends Fragment {
+
+    private MainActivity parent;
+    private View view;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_chats, container, false);
+        view =  inflater.inflate(R.layout.fragment_chats, container, false);
+
+        parent = (MainActivity) getActivity();
+
+        FloatingActionButton newChatFab = view.findViewById(R.id.newChatFab);
+
+        newChatFab.setOnClickListener(v -> newChat());
+
+        return view;
+    }
+
+    private void newChat() {
     }
 }
