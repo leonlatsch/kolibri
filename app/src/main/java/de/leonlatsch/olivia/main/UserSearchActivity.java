a62cde8e7c7b3a4cd2250f8401d856070079f3b1
Leon Latsch <leonlatsch@gmx.de>
2019-08-10 12:02:37 +0200

    added some layout fort the search -> backend work ahead

--- app/src/main/java/de/leonlatsch/olivia/main/UserSearchActivity.java
+++ app/src/main/java/de/leonlatsch/olivia/main/UserSearchActivity.java
@@ -1,8 +1,11 @@
 package de.leonlatsch.olivia.main;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
 
 import de.leonlatsch.olivia.R;
 
@@ -12,5 +15,22 @@ public class UserSearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_search);
+
+        Toolbar toolbar = findViewById(R.id.user_search_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
