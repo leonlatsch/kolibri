6e0f906019298f530b1a89b937d43de3e55dc639
Leon Latsch <dev.leonlatsch@gmail.com>
2019-09-23 15:36:13 +0200

    added no wrap

--- app/src/main/java/de/leonlatsch/olivia/util/Base64.java
+++ app/src/main/java/de/leonlatsch/olivia/util/Base64.java
@@ -3,10 +3,10 @@ package de.leonlatsch.olivia.util;
 public class Base64 {
 
     public static String toBase64(byte[] bytes) {
-        return android.util.Base64.encodeToString(bytes, 0);
+        return android.util.Base64.encodeToString(bytes, android.util.Base64.NO_WRAP);
     }
 
     public static byte[] toBytes(String base64) {
-        return android.util.Base64.decode(base64, 0);
+        return android.util.Base64.decode(base64, android.util.Base64.NO_WRAP);
     }
 }
