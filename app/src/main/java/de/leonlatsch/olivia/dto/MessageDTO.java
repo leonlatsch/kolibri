6c37f38bccb1a7448ca57303574a4a98f0554b29
Leon Latsch <dev.leonlatsch@gmail.com>
2019-10-01 01:41:07 +0200

    added chat service

---
+++ app/src/main/java/de/leonlatsch/olivia/dto/MessageDTO.java
@@ -0,0 +1,82 @@
+package de.leonlatsch.olivia.dto;
+
+import java.sql.Timestamp;
+
+public class MessageDTO {
+
+    private String mid;
+    private int from;
+    private int to;
+    private String type;
+    private Timestamp timestamp;
+    private String content;
+    private String cid;
+
+    public MessageDTO() {}
+
+    public MessageDTO(String mid, int from, int to, String type, Timestamp timestamp, String content, String cid) {
+        this.mid = mid;
+        this.from = from;
+        this.to = to;
+        this.type = type;
+        this.timestamp = timestamp;
+        this.content = content;
+        this.cid = cid;
+    }
+
+    public String getMid() {
+        return mid;
+    }
+
+    public void setMid(String mid) {
+        this.mid = mid;
+    }
+
+    public int getFrom() {
+        return from;
+    }
+
+    public void setFrom(int from) {
+        this.from = from;
+    }
+
+    public int getTo() {
+        return to;
+    }
+
+    public void setTo(int to) {
+        this.to = to;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getCid() {
+        return cid;
+    }
+
+    public void setCid(String cid) {
+        this.cid = cid;
+    }
+}
---
+++ app/src/main/java/de/leonlatsch/olivia/rest/service/ChatService.java
@@ -0,0 +1,15 @@
+package de.leonlatsch.olivia.rest.service;
+
+import de.leonlatsch.olivia.dto.Container;
+import de.leonlatsch.olivia.dto.MessageDTO;
+import de.leonlatsch.olivia.rest.http.Headers;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Header;
+import retrofit2.http.POST;
+
+public interface ChatService {
+
+    @POST("chat/send")
+    Call<Container<String>> send(@Header(Headers.ACCESS_TOKEN) String accessToken, @Body MessageDTO message);
+}
