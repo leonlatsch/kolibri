62847da25454ac27339eb45254b1f344325e5023
Leon Latsch <dev.leonlatsch@gmail.com>
2019-11-10 22:03:53 +0100

    created MessageListAdapter and binded mock messages to it.

--- app/src/main/java/de/leonlatsch/olivia/chat/ChatActivity.java
+++ app/src/main/java/de/leonlatsch/olivia/chat/ChatActivity.java
@@ -1,17 +1,25 @@
 package de.leonlatsch.olivia.chat;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
 import de.leonlatsch.olivia.R;
-import de.leonlatsch.olivia.broker.MessageConsumer;
-import de.leonlatsch.olivia.broker.MessageListener;
-import de.leonlatsch.olivia.rest.dto.MessageDTO;
+import de.leonlatsch.olivia.database.model.Message;
 
 public class ChatActivity extends AppCompatActivity {
+
+    private RecyclerView messageRecycler;
+    private MessageListAdapter messageListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +30,17 @@ public class ChatActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        List<Message> messageList = new ArrayList<>(); // Mock data for test
+        messageList.add(new Message("qhsgfdeagde", "abcde", "be414f01-989a-47de-8b5f-b6642a58dec3", "ae414f01-989a-47de-8b5f-b6642a58dec3", "TEXT", new Timestamp(21426), "Hallo ich bims"));
+        messageList.add(new Message("shdfgssfjgh", "abcde", "ae414f01-989a-47de-8b5f-b6642a58dec3", "be414f01-989a-47de-8b5f-b6642a58dec4", "TEXT", new Timestamp(21426), "Ich auch"));
+        messageList.add(new Message("dsfhdfahjdl", "abcde", "be414f01-989a-47de-8b5f-b6642a58dec3", "ae414f01-989a-47de-8b5f-b6642a58dec4", "TEXT", new Timestamp(21426), "Cool"));
+        messageList.add(new Message("asfagdadsge", "abcde", "be414f01-989a-47de-8b5f-b6642a58dec3", "ae414f01-989a-47de-8b5f-b6642a58dec4", "TEXT", new Timestamp(21426), "Ich bin ein fisch"));
+
+        messageRecycler = findViewById(R.id.chat_recycler_view);
+        messageListAdapter = new MessageListAdapter(this, messageList);
+        messageRecycler.setLayoutManager(new LinearLayoutManager(this));
+        messageRecycler.setAdapter(messageListAdapter);
     }
 
     @Override
---
+++ app/src/main/java/de/leonlatsch/olivia/chat/MessageListAdapter.java
@@ -0,0 +1,115 @@
+package de.leonlatsch.olivia.chat;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import de.leonlatsch.olivia.R;
+import de.leonlatsch.olivia.database.interfaces.UserInterface;
+import de.leonlatsch.olivia.database.model.Message;
+
+public class MessageListAdapter extends RecyclerView.Adapter {
+
+    private static final int VIEW_TYPE_MESSAGE_SENT = 1;
+    private static final int VIEW_TYPE_MESSAGE_RECEIVED = 2;
+
+    private Context mContext;
+    private List<Message> mMessageList;
+
+    private UserInterface userInterface;
+
+    public MessageListAdapter(Context context, List<Message> messageList) {
+        this.mContext = context;
+        this.mMessageList = messageList;
+        userInterface = UserInterface.getInstance();
+    }
+
+    @Override
+    public int getItemCount() {
+        return mMessageList.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        Message message = mMessageList.get(position);
+
+        if (message.getFrom().equals(userInterface.getUser().getUid())) {
+            return VIEW_TYPE_MESSAGE_SENT; // If the message from id is equal to the logged in user
+        } else {
+            return VIEW_TYPE_MESSAGE_RECEIVED;
+        }
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = null;
+
+        if (viewType == VIEW_TYPE_MESSAGE_SENT) {
+            view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_message_sent, parent, false);
+            return new SentMessageHolder(view);
+        } else if (viewType == VIEW_TYPE_MESSAGE_RECEIVED) {
+            view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_message_received, parent, false);
+            return new ReceivedMessageHolder(view);
+        }
+
+        return null; // should never happen case
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        Message message = mMessageList.get(position);
+
+        switch (holder.getItemViewType()) {
+            case VIEW_TYPE_MESSAGE_SENT:
+                ((SentMessageHolder) holder).bind(message);
+                break;
+            case VIEW_TYPE_MESSAGE_RECEIVED:
+                ((ReceivedMessageHolder) holder).bind(message);
+                break;
+        }
+    }
+
+    private class SentMessageHolder extends RecyclerView.ViewHolder {
+
+        TextView messageBody, messageTimestamp;
+
+        public SentMessageHolder(@NonNull View itemView) {
+            super(itemView);
+
+            messageBody = itemView.findViewById(R.id.sent_message_body);
+            messageTimestamp = itemView.findViewById(R.id.sent_message_timestamp);
+        }
+
+        void bind(Message message) {
+            messageBody.setText(message.getContent()); //TODO: implement decoding and decryption in MessageConsumer
+            messageTimestamp.setText(message.getTimestamp().toString().substring(11, 16)); // Hard code for the moment
+        }
+    }
+
+    private class ReceivedMessageHolder extends RecyclerView.ViewHolder {
+
+        TextView messageBody, messageTimestamp;
+
+        public ReceivedMessageHolder(@NonNull View itemView) {
+            super(itemView);
+
+            messageBody = itemView.findViewById(R.id.received_message_body);
+            messageTimestamp = itemView.findViewById(R.id.received_message_timestamp);
+        }
+
+        void bind(Message message) {
+            messageBody.setText(message.getContent()); //TODO: implement decoding and decryption in MessageConsumer
+            messageTimestamp.setText(message.getTimestamp().toString().substring(11, 16)); // Hard code for the moment
+        }
+    }
+}
