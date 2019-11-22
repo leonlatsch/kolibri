9896eb3f88f7eb98bfe8edc83a9fde0f4e6edce8
Leon Latsch <dev.leonlatsch@gmail.com>
2019-11-22 07:31:06 +0100

    Improved base Job Structure

--- app/src/main/java/dev/leonlatsch/olivia/boot/jobs/UpdateUserJob.java
+++ app/src/main/java/dev/leonlatsch/olivia/boot/jobs/UpdateUserJob.java
@@ -2,15 +2,23 @@ package dev.leonlatsch.olivia.boot.jobs;
 
 import android.content.Context;
 
+import dev.leonlatsch.olivia.database.interfaces.UserInterface;
+import dev.leonlatsch.olivia.rest.service.RestServiceFactory;
+import dev.leonlatsch.olivia.rest.service.UserService;
+
 public class UpdateUserJob extends Job {
+
+    private UserService userService;
+    private UserInterface userInterface;
 
     public UpdateUserJob(Context context) {
         super(context);
+        userInterface = UserInterface.getInstance();
+        userService = RestServiceFactory.getUserService();
     }
 
     @Override
     void execute(JobResultCallback jobResultCallback) {
-        System.out.println("abc");
-        jobResultCallback.onResult(new JobResult<String>(true, "abc"));
+        userInterface.loadUser();
     }
 }
