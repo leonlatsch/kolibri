114e632c9c04c41e6842279482d1e481d37c7887
Leon Latsch <dev.leonlatsch@gmail.com>
2019-12-18 13:45:41 +0100

    added androidx preference screen

--- app/src/main/java/dev/leonlatsch/olivia/main/fragment/SettingsFragment.java
+++ app/src/main/java/dev/leonlatsch/olivia/main/fragment/SettingsFragment.java
@@ -1,17 +1,10 @@
 package dev.leonlatsch.olivia.main.fragment;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
+import androidx.preference.PreferenceFragmentCompat;
 
 import dev.leonlatsch.olivia.R;
-import dev.leonlatsch.olivia.settings.Config;
 
 /**
  * Fragment for the setting
@@ -19,17 +12,10 @@ import dev.leonlatsch.olivia.settings.Config;
  * @author Leon Latsch
  * @since 1.0.0
  */
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends PreferenceFragmentCompat {
 
-    private SharedPreferences preferences;
-    private SharedPreferences.Editor editor;
-
-    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        preferences = Config.getSharedPreferences(getContext());
-        editor = preferences.edit();
-
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.settings, rootKey);
     }
 }
